0e06040d29f38186cd4c04dc186eb8d4df093fbc
Claudyu04 <81428418+Claudyu04@users.noreply.github.com>
2021-07-03 15:03:15 +0100

    search card added

--- client/src/components/SearchCard/index.js
+++ client/src/components/SearchCard/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function SearchCard() {
+export default function SearchCard(props) {
     return (
-        <div>
-            
-        </div>
+        
+          <div className='card'>
+          <div className='card-body'>
+        <h5 className='card-title'>About</h5>
+        <p className='card-text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quo
+          tempore, enim sunt repudiandae aspernatur amet ad saepe quod commodi.
+          Quisquam sint eaque asperiores repellat perspiciatis accusantium eius,
+          ratione fugit.
+        </p>
+        <input onChange={props.handleSearch}></input>
+        <button>Search</button>
+      </div>
+    </div> 
+     
     )
 }
