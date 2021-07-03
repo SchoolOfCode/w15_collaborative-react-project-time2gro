e6aef18e382e7625db6d6190e4c33c9c721919d2
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-07-03 14:33:43 +0100

    NoButtoncard added and Bootstrap

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,29 +1,32 @@
-import React, {useState} from 'react';
-import logo from '../../logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import HomePage from '../HomePage';
 import VegetablePage from '../VegetablePage';
 
+import logo from '../../logo.svg';
+import './App.css';
 
 function App() {
   const [isHomePage, setIsHomePage] = useState(true);
   const [isVegetablePage, setIsVegetablePage] = useState(false);
-  function handleClick(){
-    setIsHomePage(!isHomePage)
-    setIsVegetablePage(!isVegetablePage)
+  function handleClick() {
+    setIsHomePage(!isHomePage);
+    setIsVegetablePage(!isVegetablePage);
   }
-  let pageToDisplay 
-  if (isHomePage === true){
-    pageToDisplay = <HomePage/>
+  let pageToDisplay;
+  if (isHomePage === true) {
+    pageToDisplay = <HomePage />;
   }
-  if (isVegetablePage === true){
-    pageToDisplay = <VegetablePage/>
+  if (isVegetablePage === true) {
+    pageToDisplay = <VegetablePage />;
   }
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello</h1>
       {pageToDisplay}
-      <button onClick = {handleClick}>Change page</button>
+      <button onClick={handleClick}>Change page</button>
+      <button type='button' className='btn btn-primary'>
+        Primary
+      </button>
     </div>
   );
 }
