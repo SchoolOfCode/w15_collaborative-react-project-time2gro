e004405b82d2afe2e970dcecea04ec6886d5d709
natpinnock <natpinnock@gmail.com>
2021-07-05 16:55:50 +0100

    object-fit: cover added to image

--- client/src/components/NoButtonCard/index.js
+++ client/src/components/NoButtonCard/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import aboutImg from '../../img/about-banner.png';
+import React from "react";
+import aboutImg from "../../img/about-banner.png";
 
 export default function NoButtonCard(props) {
   let text;
   if (props.detailed) {
     text = (
-      <div className='card-body'>
-        <h5 className='card-title'>{props.heading}</h5>
-        <h6 className='card-subtitle text-muted'>{props.subheadingOne}</h6>
-        <p className='card-text'>{props.paragraphOne}</p>
-        <h6 className='card-subtitle text-muted'>{props.subheadingTwo}</h6>
-        <p className='card-text'>{props.paragraphTwo}</p>
-        <h6 className='card-subtitle text-muted'>{props.subheadingThree}</h6>
-        <p className='card-text'>{props.paragraphThree}</p>
+      <div className="card-body">
+        <h5 className="card-title">{props.heading}</h5>
+        <h6 className="card-subtitle text-muted">{props.subheadingOne}</h6>
+        <p className="card-text">{props.paragraphOne}</p>
+        <h6 className="card-subtitle text-muted">{props.subheadingTwo}</h6>
+        <p className="card-text">{props.paragraphTwo}</p>
+        <h6 className="card-subtitle text-muted">{props.subheadingThree}</h6>
+        <p className="card-text">{props.paragraphThree}</p>
       </div>
     );
   } else {
     text = (
-      <div className='card-body'>
-        <h5 className='card-title'>{props.heading}</h5>
-        <p className='card-text'>{props.paragraph}</p>
+      <div className="card-body">
+        <h5 className="card-title">{props.heading}</h5>
+        <p className="card-text">{props.paragraph}</p>
       </div>
     );
   }
@@ -28,15 +28,12 @@ export default function NoButtonCard(props) {
     <div className="card border-secondary mb-3">
       <img
         className="card-img-top"
+        style={{ objectFit: "cover", objectPosition: "40% 5%" }}
         src={aboutImg}
         alt="vegetables"
-        height="150px"
-        width="300px"
+        height="200px"
       ></img>
-      <div className="card-body">
-        {text}
-      </div>
-
+      <div className="card-body">{text}</div>
     </div>
   );
 }
