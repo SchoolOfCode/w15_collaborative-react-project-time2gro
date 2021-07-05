6867518ab2c72116a0544d9497da07f33163b675
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-07-05 21:32:33 +0100

    question page initialised

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import HomePage from '../HomePage';
 import VegetablePage from '../VegetablePage';
+import QuestionPage from '../QuestionPage';
 
 import logo from '../../logo.svg';
+import { answersArray } from '../../utils/text';
 import './App.css';
 
 function App() {
   const [isHomePage, setIsHomePage] = useState(true);
   const [isVegetablePage, setIsVegetablePage] = useState(false);
+  const [isQuestionPage, setIsQuestionPage] = useState(false);
   const [vegetableToSearch, setVegetableToSearch] = useState('');
   const [apiData, setApiData] = useState([]);
   const [currentVegetable, setCurrentVegetable] = useState({});
+  const [answers, setAnswers] = useState(answersArray);
+  const [userSpace, setUserSpace] = useState('');
   useEffect(() => {
     async function fetchVegetable() {
       const requestUrl = await fetch(
@@ -43,6 +48,13 @@ function App() {
     setIsVegetablePage(true);
   }
 
+  function handleAnswerClick(e) {
+    console.log(e.target.dataset.button);
+    setUserSpace(e.target.dataset.button);
+    setIsQuestionPage(false);
+    setIsVegetablePage(true);
+  }
+
   let pageToDisplay;
   if (isHomePage === true) {
     pageToDisplay = (
@@ -58,6 +70,15 @@ function App() {
       <VegetablePage
         currentVegetable={currentVegetable}
         handleHomeClick={handleHomeClick}
+      />
+    );
+  }
+  if (isQuestionPage === true) {
+    pageToDisplay = (
+      <QuestionPage
+        answers={answers}
+        questionNumber='1'
+        handleClick={handleAnswerClick}
       />
     );
   }
--- client/src/components/NoButtonCard/index.js
+++ client/src/components/NoButtonCard/index.js
@@ -1,39 +1,39 @@
-import React from "react";
-import aboutImg from "../../img/about-banner.png";
+import React from 'react';
+import aboutImg from '../../img/about-banner.png';
 
 export default function NoButtonCard(props) {
   let text;
   if (props.detailed) {
     text = (
-      <div className="card-body">
-        <h5 className="card-title">{props.heading}</h5>
-        <h6 className="card-subtitle text-muted">{props.subheadingOne}</h6>
-        <p className="card-text">{props.paragraphOne}</p>
-        <h6 className="card-subtitle text-muted">{props.subheadingTwo}</h6>
-        <p className="card-text">{props.paragraphTwo}</p>
-        <h6 className="card-subtitle text-muted">{props.subheadingThree}</h6>
-        <p className="card-text">{props.paragraphThree}</p>
+      <div className='card-body'>
+        <h5 className='card-title'>{props.heading}</h5>
+        <h6 className='card-subtitle text-muted'>{props.subheadingOne}</h6>
+        <p className='card-text'>{props.paragraphOne}</p>
+        <h6 className='card-subtitle text-muted'>{props.subheadingTwo}</h6>
+        <p className='card-text'>{props.paragraphTwo}</p>
+        <h6 className='card-subtitle text-muted'>{props.subheadingThree}</h6>
+        <p className='card-text'>{props.paragraphThree}</p>
       </div>
     );
   } else {
     text = (
-      <div className="card-body">
-        <h5 className="card-title">{props.heading}</h5>
-        <p className="card-text">{props.paragraph}</p>
+      <div className='card-body'>
+        <h5 className='card-title'>{props.heading}</h5>
+        <p className='card-text'>{props.paragraph}</p>
       </div>
     );
   }
 
   return (
-    <div className="card border-secondary mb-3">
+    <div className='card border-secondary mb-3'>
       <img
-        className="card-img-top"
-        style={{ objectFit: "cover", objectPosition: "40% 5%" }}
+        className='card-img-top'
+        style={{ objectFit: 'cover', objectPosition: '40% 5%' }}
         src={aboutImg}
-        alt="vegetables"
-        height="200px"
+        alt='vegetables'
+        height='200px'
       ></img>
-      <div className="card-body">{text}</div>
+      <div className='card-body'>{text}</div>
     </div>
   );
 }
