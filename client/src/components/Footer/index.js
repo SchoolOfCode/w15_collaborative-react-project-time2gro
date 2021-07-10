2aadbebc66609a7bc309b0bc5e18b1bb4ba76009
RyanBrown870 <18215428+RyanBrown870@users.noreply.github.com>
2021-07-10 17:45:54 +0100

    updated about section

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { footerText } from '../../utils/text';
+import './footer.css';
 
 export default function Footer() {
   return (
     <footer>
-      <p>Footer</p>
-      <p>Goes here</p>
-
+      <p className='text-muted spacing'>{footerText}</p>
     </footer>
   );
 }
